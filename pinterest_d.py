e10e1a5986b5361fbcd90a21b55e7296eaf38779
Niro <92976312+NodirCoder@users.noreply.github.com>
2024-04-25 10:03:28 +0500

    Add files via upload

---
+++ pinterest_d.py
@@ -0,0 +1,23 @@
+from requests_html import HTMLSession
+
+ZORO_IMG = 'https://pin.it/4zLa0ig5l'
+s = HTMLSession()
+
+def get_pin(url: str):
+    try:
+        resp = s.get(url)
+        
+        el = resp.html.find('img.hCL.kVc.L4E.MIw')
+        img_url = el[0].attrs['src']
+        print(el[0].attrs['src']) # PRPRPRPRPRP
+        
+        resp = s.get(img_url)
+        with open('image.jpg', 'wb+') as img:
+            img.write(resp.content)
+    except Exception as e:
+        print(e)
+    
+
+if __name__ == '__main__':
+    pin_url = str(input("[!]Enter a link: "))
+    get_pin(pin_url)
